5e0569673f2a63c9809626f9887d61a885421059
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-07-19 11:48:04 +0530

    created log groups

--- modules/cloudwatch/main.tf
+++ modules/cloudwatch/main.tf
@@ -10,13 +10,6 @@ resource "aws_cloudwatch_log_group" "appointment_service" {
   retention_in_days = 7
 }
 
-
-#resource "aws_cloudwatch_log_stream" "ecs_log_stream" {
-#  log_group_name = aws_cloudwatch_log_group.ecs_log_group.name
-#  name           = "${var.name}-stream"
-#}
-
-
 output "patient_service_log_group_name" {
   description = "The name of the CloudWatch log group for the patient service"
   value       = aws_cloudwatch_log_group.patient_service.name
